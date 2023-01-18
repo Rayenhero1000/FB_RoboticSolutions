6ed2ad5b1ee21baa7568e3d2d71ae0842e9b736a
XeroTheProtogen <db318330@gmail.com>
2023-01-19 09:56:04 +1300

    Energy simplification

---
+++ src/main/java/net/demoman/roboticsolutions/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package net.demoman.roboticsolutions;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
